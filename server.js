a2f2bb5178dcd09c1fb80c9251cb954aca1cd16f
giuliobosco <giuliobva@gmail.com>
2020-01-06 17:01:36 +0100

    add base http server

---
+++ server.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const express = require('express');
+
+// Constatns 
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+const app = express();
+app.get('/', (req, res) => {
+	res.send('Hello world\n');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
